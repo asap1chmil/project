5919665f2611ea2b42bc2000f401fc1b9e585dac
Artem <artem.chmil@bk.ru>
2022-06-27 17:36:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// "use strict";
+//  let number = 10; 
+// console.log(number);
+// let COLOR_RED = '#F00';
+
+// let userName = 'John,';
+// let number = 25;
+// number = 24;
+// console.log(userName, 'Old: ' + number, 'years');
+// let storeName = 'New Balance';
+// const storeDescription = {
+//     budget: 10000,
+//     employeesS: ['Popik', 'Volodya', 'Jeka'],
+//     products: {
+//         'Milk': 100,
+//         'Sugar': 50
+//     }
+//     openStore: false
+// };
+alert(1);
